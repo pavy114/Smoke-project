0963e379ba33e1690972e9c901f329f6c07164d3
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-20 14:29:14 +0530

    commit

---
+++ src/test/java/Sample3/SmokeTest.java
@@ -0,0 +1,14 @@
+package Sample3;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+	
+	
+	@Test
+	public void Smokecontact()
+	{
+		System.out.println("Insidfdgfgde smoke create contact....");
+	}
+
+}
